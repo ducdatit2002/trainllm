da389a2bd0e22439ea2b0ab91b3b69ad362a705c
Pham Duc Dat <phamducdat@192.168.1.243>
2024-05-12 13:18:22 +0700

    push sh

--- train.sh
+++ train.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#SBATCH --job-name=train.sh
+#SBATCH --job-name=trainllm
 #SBATCH --nodes=1                
 #SBATCH --ntasks=4   
 #SBATCH --cpus-per-task=8     
 #SBATCH --mem-per-cpu=64G
-#SBATCH --partition=gtx1080 
-#SBATCH --gres=gpu:4      
+#SBATCH --partition=a100 
+#SBATCH --gres=gpu:2     
 #SBATCH --time=2-00:00:00        
 #SBATCH --output=train_%j.out  
 #SBATCH --error=train_%j.err    
